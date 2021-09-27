e2a26c2a10c31c2eb171f1eb700e99bc3bf9b787
CosmicBee <bmhcanada@gmail.com>
2021-09-26 17:07:11 -0700

    color
    changed the button hover color and also deleted p1 so it could use the html css

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //Crypto wallet alert
 
 function myFunction() {
-    alert("Thank you🙏for the ramen🍜:0x6BD06B139f44cac8652D158567C3F13c772a3bdE  ");
+    alert("Thank you🙏for the ramen🍜 Wallet: 0x6BD06B139f44cac8652D158567C3F13c772a3bdE  ");
   }
 
   function myD() {
-    alert("Thank you your kindness will bring many 🐼bears🐻 a brighter future✨:W0xC1bD9881120aDbdE106CB6d1D75dEe7c87EA1137 ");
+    alert("Thank you your kindness will bring many 🐼bears🐻 a brighter future✨ Wallet: W0xC1bD9881120aDbdE106CB6d1D75dEe7c87EA1137 ");
   }
 
 
   function myGame() {
-    alert("Thank you for helping get this community to the next level🎮:0x5B76D4CBF1a3FF4c4Cd201a7a82a4BF6B17c7C9f  ");
+    alert("Thank you for helping get this community to the next level🎮 Wallet: 0x5B76D4CBF1a3FF4c4Cd201a7a82a4BF6B17c7C9f  ");
   }
 
 
